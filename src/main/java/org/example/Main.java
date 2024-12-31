d10c13832a12c54a9d88b6669e38c5a83eecace8
Anurag <uanurag216@gmail.com>
2024-12-31 23:16:24 +0530

    added commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,27 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        int maxDay = days[days.length - 1];
+        boolean[] travelDay = new boolean[maxDay + 1];
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int day : days)
+            travelDay[day] = true;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int[] dp = new int[maxDay + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= maxDay; i++) {
+            if (!travelDay[i]) {
+                dp[i] = dp[i - 1];
+                continue;
+            }
+
+            dp[i] = costs[0] + dp[i - 1];
+            dp[i] = Math.min(dp[Math.max(0, i - 7)] + costs[1], dp[i]);
+            dp[i] = Math.min(dp[Math.max(0, i - 30)] + costs[2], dp[i]);
         }
+
+        return dp[maxDay];
     }
 }
